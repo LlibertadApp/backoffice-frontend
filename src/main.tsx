8cd3cbe2d3fcf379632027b1e32e356cf4b02ffe
Ignacio Martin Vazquez <Vazquez_Ignacio_Martin@hotmail.com>
2023-11-15 23:54:03 -0300

    router, dashboard y login

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { NextUIProvider } from '@nextui-org/react'
+import AppRouter from './routes/router.tsx'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+     <NextUIProvider>
+      <BrowserRouter>
+     <main className="dark text-foreground bg-background w-screen h-screen">
+        <AppRouter />
+        </main>
+      </BrowserRouter>
+
+     </NextUIProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,13 @@
+
+
+const Dashboard = () => {
+
+
+
+    return <div className="flex items-center justify-center flex-col max-w-md">
+    <span>Dashboard</span>
+
+    </div>
+}
+
+export default Dashboard
