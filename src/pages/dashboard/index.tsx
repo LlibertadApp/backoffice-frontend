5a5bea448331fd86d3583326df46978e55da0239
Ignacio Martin Vazquez <Vazquez_Ignacio_Martin@hotmail.com>
2023-11-16 12:35:11 -0300

    search

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,7 @@
 import {
     Button,
     Chip,
+    Input,
     Pagination,
     Snippet,
     Table,
@@ -19,6 +20,8 @@ import EditIcon from '../../assets/EditIcon';
 import DeleteIcon from '../../assets/DeleteIcon';
 import { dummyFiscales } from './data';
 import { Fiscal } from '../../entities/Fiscal';
+import { PlusIcon } from '../../assets/PlusIcon';
+import { SearchIcon } from '../../assets/SearchIcon';
 
 const columns = [
     { name: 'Nombre Completo', uid: 'fullName' },
@@ -35,6 +38,8 @@ const statusColorMap = {
 
 const Dashboard = () => {
     const { isOpen, onOpen, onOpenChange } = useDisclosure();
+    const [filterValue, setFilterValue] = useState('');
+    const hasSearchFilter = Boolean(filterValue);
 
     const [deleteFiscalID, setDeleteFiscalID] = useState<string>('');
     const openDeleteFiscal = (fiscalID: string) => {
@@ -64,13 +69,15 @@ const Dashboard = () => {
                         <Snippet symbol="" codeString={fiscal.magicLink}>
                             Copiar Link
                         </Snippet>
-                        <Tooltip content="Editar Fiscal">
-                            <Link to={'/edit-fiscal'}>
+
+                        <Link to={'/edit-fiscal'} className="text-black">
+                            <Tooltip color="secondary" content="Editar Fiscal">
                                 <span className="text-xl text-default-400 cursor-pointer active:opacity-50">
                                     <EditIcon />
                                 </span>
-                            </Link>
-                        </Tooltip>
+                            </Tooltip>
+                        </Link>
+
                         <Tooltip color="danger" content="Eliminar Fiscal">
                             <span className="text-xl text-danger cursor-pointer active:opacity-50" onClick={() => openDeleteFiscal(fiscal.id)}>
                                 <DeleteIcon />
@@ -83,54 +90,95 @@ const Dashboard = () => {
         }
     }, []);
 
+    const filteredItems = useMemo(() => {
+        let filteredUsers = [...dummyFiscales];
+
+        if (hasSearchFilter) {
+            filteredUsers = filteredUsers.filter((fiscal) => fiscal.fullName.toLowerCase().includes(filterValue.toLowerCase()));
+        }
+
+        return filteredUsers;
+    }, [dummyFiscales, filterValue]);
+
+    const onSearchChange = useCallback((value: string) => {
+        if (value) {
+            setFilterValue(value);
+            setPage(1);
+        } else {
+            setFilterValue('');
+        }
+    }, []);
+
+    const onClear = useCallback(() => {
+        setFilterValue('');
+        setPage(1);
+    }, []);
+
     const [page, setPage] = useState(1);
     const rowsPerPage = 4;
-
-    const pages = Math.ceil(dummyFiscales.length / rowsPerPage);
+    const pages = Math.ceil(filteredItems.length / rowsPerPage);
 
     const items = useMemo(() => {
         const start = (page - 1) * rowsPerPage;
         const end = start + rowsPerPage;
 
-        return dummyFiscales.slice(start, end);
-    }, [page]);
+        return filteredItems.slice(start, end);
+    }, [page, filteredItems]);
+
+    const topContent = useMemo(() => {
+        return (
+            <div className="flex justify-between gap-3 items-end ">
+                <Input
+                    size="sm"
+                    isClearable
+                    className="w-full sm:max-w-[44%]"
+                    placeholder="Buscar por nombre"
+                    startContent={<SearchIcon />}
+                    value={filterValue}
+                    onClear={() => onClear()}
+                    onValueChange={onSearchChange}
+                />
+                <Link to={'/new-fiscal'}>
+                    <Button color="primary" size="lg" endContent={<PlusIcon />}>
+                        Añadir Fiscal
+                    </Button>
+                </Link>
+            </div>
+        );
+    }, [filterValue, dummyFiscales.length, onSearchChange, hasSearchFilter]);
 
     return (
-        <div className="flex items-center justify-center flex-col">
-            <span>Dashboard</span>
-            <Link to={'/new-fiscal'}>
-                <Button color="primary">+ Añadir Fiscal</Button>
-            </Link>
-            <div>
-                Fiscales
-                <Table
-                    aria-label="Example table with custom cells"
-                    bottomContent={
-                        <div className="flex w-full justify-center">
-                            <Pagination
-                                isCompact
-                                showControls
-                                showShadow
-                                color="secondary"
-                                page={page}
-                                total={pages}
-                                onChange={(page) => setPage(page)}
-                            />
-                        </div>
-                    }
-                >
-                    <TableHeader columns={columns}>
-                        {(column) => (
-                            <TableColumn key={column.uid} align={column.uid === 'actions' ? 'center' : 'start'}>
-                                {column.name}
-                            </TableColumn>
-                        )}
-                    </TableHeader>
-                    <TableBody items={items}>
-                        {(item) => <TableRow key={item.id}>{(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}</TableRow>}
-                    </TableBody>
-                </Table>
-            </div>
+        <div className="w-screen h-screen p-8 flex items-start justify-center">
+            <Table
+                aria-label="Example table with custom cells"
+                topContent={topContent}
+                topContentPlacement="outside"
+                bottomContent={
+                    <div className="flex w-full justify-center">
+                        <Pagination
+                            isCompact
+                            showControls
+                            showShadow
+                            color="secondary"
+                            page={page}
+                            total={pages}
+                            onChange={(page) => setPage(page)}
+                        />
+                    </div>
+                }
+            >
+                <TableHeader columns={columns}>
+                    {(column) => (
+                        <TableColumn key={column.uid} align={column.uid === 'actions' ? 'center' : 'start'}>
+                            {column.name}
+                        </TableColumn>
+                    )}
+                </TableHeader>
+                <TableBody items={items}>
+                    {(item) => <TableRow key={item.id}>{(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}</TableRow>}
+                </TableBody>
+            </Table>
+
             <DeleteFiscalModal onOpenChange={onOpenChange} isOpen={isOpen} fiscalID={deleteFiscalID} />
         </div>
     );
