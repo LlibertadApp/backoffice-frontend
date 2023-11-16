f6201df93983c29e45faa77ebcb7ed8092e7c24f
Ignacio Martin Vazquez <Vazquez_Ignacio_Martin@hotmail.com>
2023-11-16 11:49:03 -0300

    types y paginacion

---
+++ src/pages/dashboard/data.ts
@@ -0,0 +1,112 @@
+import { Fiscal } from '../../entities/Fiscal';
+
+export const dummyFiscales: Fiscal[] = [
+    {
+        fullName: 'Ignacio Vazquez',
+        magicLink:
+            'https://app.dev.libertapp.net/?authToken=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2lkZW50aXR5dG9vbGtpdC5nb29nbGVhcGlzLmNvbS9nb29nbGUuaWRlbnRpdHkuaWRlbnRpdHl0b29sa2l0LnYxLklkZW50aXR5VG9vbGtpdCIsImlhdCI6MTcwMDExMDgzOSwiZXhwIjoxNzAwMTE0NDM5LCJpc3MiOiJmaXJlYmFzZS1hZG1pbnNkay1rNW5jdUBsaWJlcnRhcHAtZGV2LTRlYzhmLmlhbS5nc2VydmljZWFjY291bnQuY29tIiwic3ViIjoiZmlyZWJhc2UtYWRtaW5zZGstazVuY3VAbGliZXJ0YXBwLWRldi00ZWM4Zi5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsInVpZCI6ImI0NmY5MDRkLWFjNjgtNDVkMi1hMGM5LTc2MWFmMGZhY2Y5ZCIsImNsYWltcyI6eyJtZXNhcyI6W3sibWVzYUlkIjoiMDAwMDEifSx7Im1lc2FJZCI6IjAwMDAyIn0seyJtZXNhSWQiOiIwMDAwMyJ9LHsibWVzYUlkIjoiMDAwMDQifSx7Im1lc2FJZCI6IjAwMDA1In1dfX0.E75bnM2hIfrleNZUtK-GShln38O1TXgxpb60-LV2mcEsM0pxMk9p7xNWZdeblyQ12opL6zAlU9dCouWhGPVS0gej9JfliPrUeJ9LlcRJyvCkUnxL8OyHtFmFLSGzej0WdD-YLWWv2MuSYNA9XARFZgmI3r7eKTfwRWAewZPZCRRxAI0kzI4uF24pa3XIK6p5hDjBnJG00QAu2L_DtVTiVL1eUsg31jQAYOl4SLcIzV1kxJy5HWHDebUVwn-jlKiMTS1pNV6zO4A2Id_1nP28xARu8V2xxwBbA6UouYkB_6uWiVHQwtmQ3WNcyIhmDKBf_L4aqJWBziTIiUCeIQFAYw',
+        id: '0323-12343-1234-00123B',
+        status: 'registered',
+        email: 'ignasave39@gmail.com',
+        phone: '1203120312',
+    },
+    {
+        fullName: 'Leila Gonzalez',
+        magicLink:
+            'https://app.dev.libertapp.net/?authToken=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2lkZW50aXR5dG9vbGtpdC5nb29nbGVhcGlzLmNvbS9nb29nbGUuaWRlbnRpdHkuaWRlbnRpdHl0b29sa2l0LnYxLklkZW50aXR5VG9vbGtpdCIsImlhdCI6MTcwMDExMDgzOSwiZXhwIjoxNzAwMTE0NDM5LCJpc3MiOiJmaXJlYmFzZS1hZG1pbnNkay1rNW5jdUBsaWJlcnRhcHAtZGV2LTRlYzhmLmlhbS5nc2VydmljZWFjY291bnQuY29tIiwic3ViIjoiZmlyZWJhc2UtYWRtaW5zZGstazVuY3VAbGliZXJ0YXBwLWRldi00ZWM4Zi5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsInVpZCI6ImI0NmY5MDRkLWFjNjgtNDVkMi1hMGM5LTc2MWFmMGZhY2Y5ZCIsImNsYWltcyI6eyJtZXNhcyI6W3sibWVzYUlkIjoiMDAwMDEifSx7Im1lc2FJZCI6IjAwMDAyIn0seyJtZXNhSWQiOiIwMDAwMyJ9LHsibWVzYUlkIjoiMDAwMDQifSx7Im1lc2FJZCI6IjAwMDA1In1dfX0.E75bnM2hIfrleNZUtK-GShln38O1TXgxpb60-LV2mcEsM0pxMk9p7xNWZdeblyQ12opL6zAlU9dCouWhGPVS0gej9JfliPrUeJ9LlcRJyvCkUnxL8OyHtFmFLSGzej0WdD-YLWWv2MuSYNA9XARFZgmI3r7eKTfwRWAewZPZCRRxAI0kzI4uF24pa3XIK6p5hDjBnJG00QAu2L_DtVTiVL1eUsg31jQAYOl4SLcIzV1kxJy5HWHDebUVwn-jlKiMTS1pNV6zO4A2Id_1nP28xARu8V2xxwBbA6UouYkB_6uWiVHQwtmQ3WNcyIhmDKBf_L4aqJWBziTIiUCeIQFAYw',
+        id: '0323-12343-1234-00123A',
+        status: 'registered',
+        email: 'leilagonzalez@gmail.com',
+        phone: '1203120312',
+    },
+    {
+        fullName: 'Laura Rodriguez',
+        magicLink:
+            'https://app.dev.libertapp.net/?authToken=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2lkZW50aXR5dG9vbGtpdC5nb29nbGVhcGlzLmNvbS9nb29nbGUuaWRlbnRpdHkuaWRlbnRpdHl0b29sa2l0LnYxLklkZW50aXR5VG9vbGtpdCIsImlhdCI6MTcwMDExMDgzOSwiZXhwIjoxNzAwMTE0NDM5LCJpc3MiOiJmaXJlYmFzZS1hZG1pbnNkay1rNW5jdUBsaWJlcnRhcHAtZGV2LTRlYzhmLmlhbS5nc2VydmljZWFjY291bnQuY29tIiwic3ViIjoiZmlyZWJhc2UtYWRtaW5zZGstazVuY3VAbGliZXJ0YXBwLWRldi00ZWM4Zi5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsInVpZCI6ImI0NmY5MDRkLWFjNjgtNDVkMi1hMGM5LTc2MWFmMGZhY2Y5ZCIsImNsYWltcyI6eyJtZXNhcyI6W3sibWVzYUlkIjoiMDAwMDEifSx7Im1lc2FJZCI6IjAwMDAyIn0seyJtZXNhSWQiOiIwMDAwMyJ9LHsibWVzYUlkIjoiMDAwMDQifSx7Im1lc2FJZCI6IjAwMDA1In1dfX0.E75bnM2hIfrleNZUtK-GShln38O1TXgxpb60-LV2mcEsM0pxMk9p7xNWZdeblyQ12opL6zAlU9dCouWhGPVS0gej9JfliPrUeJ9LlcRJyvCkUnxL8OyHtFmFLSGzej0WdD-YLWWv2MuSYNA9XARFZgmI3r7eKTfwRWAewZPZCRRxAI0kzI4uF24pa3XIK6p5hDjBnJG00QAu2L_DtVTiVL1eUsg31jQAYOl4SLcIzV1kxJy5HWHDebUVwn-jlKiMTS1pNV6zO4A2Id_1nP28xARu8V2xxwBbA6UouYkB_6uWiVHQwtmQ3WNcyIhmDKBf_L4aqJWBziTIiUCeIQFAYw',
+        id: '0323-12343-1234-00123C',
+        status: 'pending',
+        email: 'laverodriguez@gmail.com',
+        phone: '1203120312',
+    },
+    {
+        fullName: 'Ignacio Vazquez',
+        magicLink:
+            'https://app.dev.libertapp.net/?authToken=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2lkZW50aXR5dG9vbGtpdC5nb29nbGVhcGlzLmNvbS9nb29nbGUuaWRlbnRpdHkuaWRlbnRpdHl0b29sa2l0LnYxLklkZW50aXR5VG9vbGtpdCIsImlhdCI6MTcwMDExMDgzOSwiZXhwIjoxNzAwMTE0NDM5LCJpc3MiOiJmaXJlYmFzZS1hZG1pbnNkay1rNW5jdUBsaWJlcnRhcHAtZGV2LTRlYzhmLmlhbS5nc2VydmljZWFjY291bnQuY29tIiwic3ViIjoiZmlyZWJhc2UtYWRtaW5zZGstazVuY3VAbGliZXJ0YXBwLWRldi00ZWM4Zi5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsInVpZCI6ImI0NmY5MDRkLWFjNjgtNDVkMi1hMGM5LTc2MWFmMGZhY2Y5ZCIsImNsYWltcyI6eyJtZXNhcyI6W3sibWVzYUlkIjoiMDAwMDEifSx7Im1lc2FJZCI6IjAwMDAyIn0seyJtZXNhSWQiOiIwMDAwMyJ9LHsibWVzYUlkIjoiMDAwMDQifSx7Im1lc2FJZCI6IjAwMDA1In1dfX0.E75bnM2hIfrleNZUtK-GShln38O1TXgxpb60-LV2mcEsM0pxMk9p7xNWZdeblyQ12opL6zAlU9dCouWhGPVS0gej9JfliPrUeJ9LlcRJyvCkUnxL8OyHtFmFLSGzej0WdD-YLWWv2MuSYNA9XARFZgmI3r7eKTfwRWAewZPZCRRxAI0kzI4uF24pa3XIK6p5hDjBnJG00QAu2L_DtVTiVL1eUsg31jQAYOl4SLcIzV1kxJy5HWHDebUVwn-jlKiMTS1pNV6zO4A2Id_1nP28xARu8V2xxwBbA6UouYkB_6uWiVHQwtmQ3WNcyIhmDKBf_L4aqJWBziTIiUCeIQFAYw',
+        id: '0323-12343-123aasd4-00123',
+        status: 'registered',
+        email: 'ignasave39@gmail.com',
+        phone: '1203120312',
+    },
+    {
+        fullName: 'Leila Gonzalez',
+        magicLink:
+            'https://app.dev.libertapp.net/?authToken=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2lkZW50aXR5dG9vbGtpdC5nb29nbGVhcGlzLmNvbS9nb29nbGUuaWRlbnRpdHkuaWRlbnRpdHl0b29sa2l0LnYxLklkZW50aXR5VG9vbGtpdCIsImlhdCI6MTcwMDExMDgzOSwiZXhwIjoxNzAwMTE0NDM5LCJpc3MiOiJmaXJlYmFzZS1hZG1pbnNkay1rNW5jdUBsaWJlcnRhcHAtZGV2LTRlYzhmLmlhbS5nc2VydmljZWFjY291bnQuY29tIiwic3ViIjoiZmlyZWJhc2UtYWRtaW5zZGstazVuY3VAbGliZXJ0YXBwLWRldi00ZWM4Zi5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsInVpZCI6ImI0NmY5MDRkLWFjNjgtNDVkMi1hMGM5LTc2MWFmMGZhY2Y5ZCIsImNsYWltcyI6eyJtZXNhcyI6W3sibWVzYUlkIjoiMDAwMDEifSx7Im1lc2FJZCI6IjAwMDAyIn0seyJtZXNhSWQiOiIwMDAwMyJ9LHsibWVzYUlkIjoiMDAwMDQifSx7Im1lc2FJZCI6IjAwMDA1In1dfX0.E75bnM2hIfrleNZUtK-GShln38O1TXgxpb60-LV2mcEsM0pxMk9p7xNWZdeblyQ12opL6zAlU9dCouWhGPVS0gej9JfliPrUeJ9LlcRJyvCkUnxL8OyHtFmFLSGzej0WdD-YLWWv2MuSYNA9XARFZgmI3r7eKTfwRWAewZPZCRRxAI0kzI4uF24pa3XIK6p5hDjBnJG00QAu2L_DtVTiVL1eUsg31jQAYOl4SLcIzV1kxJy5HWHDebUVwn-jlKiMTS1pNV6zO4A2Id_1nP28xARu8V2xxwBbA6UouYkB_6uWiVHQwtmQ3WNcyIhmDKBf_L4aqJWBziTIiUCeIQFAYw',
+        id: '0323-12343-1234-00asdasd123A',
+        status: 'registered',
+        email: 'leilagonzalez@gmail.com',
+        phone: '1203120312',
+    },
+    {
+        fullName: 'Laura Rodriguez',
+        magicLink:
+            'https://app.dev.libertapp.net/?authToken=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2lkZW50aXR5dG9vbGtpdC5nb29nbGVhcGlzLmNvbS9nb29nbGUuaWRlbnRpdHkuaWRlbnRpdHl0b29sa2l0LnYxLklkZW50aXR5VG9vbGtpdCIsImlhdCI6MTcwMDExMDgzOSwiZXhwIjoxNzAwMTE0NDM5LCJpc3MiOiJmaXJlYmFzZS1hZG1pbnNkay1rNW5jdUBsaWJlcnRhcHAtZGV2LTRlYzhmLmlhbS5nc2VydmljZWFjY291bnQuY29tIiwic3ViIjoiZmlyZWJhc2UtYWRtaW5zZGstazVuY3VAbGliZXJ0YXBwLWRldi00ZWM4Zi5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsInVpZCI6ImI0NmY5MDRkLWFjNjgtNDVkMi1hMGM5LTc2MWFmMGZhY2Y5ZCIsImNsYWltcyI6eyJtZXNhcyI6W3sibWVzYUlkIjoiMDAwMDEifSx7Im1lc2FJZCI6IjAwMDAyIn0seyJtZXNhSWQiOiIwMDAwMyJ9LHsibWVzYUlkIjoiMDAwMDQifSx7Im1lc2FJZCI6IjAwMDA1In1dfX0.E75bnM2hIfrleNZUtK-GShln38O1TXgxpb60-LV2mcEsM0pxMk9p7xNWZdeblyQ12opL6zAlU9dCouWhGPVS0gej9JfliPrUeJ9LlcRJyvCkUnxL8OyHtFmFLSGzej0WdD-YLWWv2MuSYNA9XARFZgmI3r7eKTfwRWAewZPZCRRxAI0kzI4uF24pa3XIK6p5hDjBnJG00QAu2L_DtVTiVL1eUsg31jQAYOl4SLcIzV1kxJy5HWHDebUVwn-jlKiMTS1pNV6zO4A2Id_1nP28xARu8V2xxwBbA6UouYkB_6uWiVHQwtmQ3WNcyIhmDKBf_L4aqJWBziTIiUCeIQFAYw',
+        id: '0323-12343-1234-0fasfasdas0123C',
+        status: 'pending',
+        email: 'laverodriguez@gmail.com',
+        phone: '1203120312',
+    },
+    {
+        fullName: 'Ignacio Vazquez',
+        magicLink:
+            'https://app.dev.libertapp.net/?authToken=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2lkZW50aXR5dG9vbGtpdC5nb29nbGVhcGlzLmNvbS9nb29nbGUuaWRlbnRpdHkuaWRlbnRpdHl0b29sa2l0LnYxLklkZW50aXR5VG9vbGtpdCIsImlhdCI6MTcwMDExMDgzOSwiZXhwIjoxNzAwMTE0NDM5LCJpc3MiOiJmaXJlYmFzZS1hZG1pbnNkay1rNW5jdUBsaWJlcnRhcHAtZGV2LTRlYzhmLmlhbS5nc2VydmljZWFjY291bnQuY29tIiwic3ViIjoiZmlyZWJhc2UtYWRtaW5zZGstazVuY3VAbGliZXJ0YXBwLWRldi00ZWM4Zi5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsInVpZCI6ImI0NmY5MDRkLWFjNjgtNDVkMi1hMGM5LTc2MWFmMGZhY2Y5ZCIsImNsYWltcyI6eyJtZXNhcyI6W3sibWVzYUlkIjoiMDAwMDEifSx7Im1lc2FJZCI6IjAwMDAyIn0seyJtZXNhSWQiOiIwMDAwMyJ9LHsibWVzYUlkIjoiMDAwMDQifSx7Im1lc2FJZCI6IjAwMDA1In1dfX0.E75bnM2hIfrleNZUtK-GShln38O1TXgxpb60-LV2mcEsM0pxMk9p7xNWZdeblyQ12opL6zAlU9dCouWhGPVS0gej9JfliPrUeJ9LlcRJyvCkUnxL8OyHtFmFLSGzej0WdD-YLWWv2MuSYNA9XARFZgmI3r7eKTfwRWAewZPZCRRxAI0kzI4uF24pa3XIK6p5hDjBnJG00QAu2L_DtVTiVL1eUsg31jQAYOl4SLcIzV1kxJy5HWHDebUVwn-jlKiMTS1pNV6zO4A2Id_1nP28xARu8V2xxwBbA6UouYkB_6uWiVHQwtmQ3WNcyIhmDKBf_L4aqJWBziTIiUCeIQFAYw',
+        id: '0323-12343-1234-001fasdasdaszxc23B',
+        status: 'registered',
+        email: 'ignasave39@gmail.com',
+        phone: '1203120312',
+    },
+    {
+        fullName: 'Leila Gonzalez',
+        magicLink:
+            'https://app.dev.libertapp.net/?authToken=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2lkZW50aXR5dG9vbGtpdC5nb29nbGVhcGlzLmNvbS9nb29nbGUuaWRlbnRpdHkuaWRlbnRpdHl0b29sa2l0LnYxLklkZW50aXR5VG9vbGtpdCIsImlhdCI6MTcwMDExMDgzOSwiZXhwIjoxNzAwMTE0NDM5LCJpc3MiOiJmaXJlYmFzZS1hZG1pbnNkay1rNW5jdUBsaWJlcnRhcHAtZGV2LTRlYzhmLmlhbS5nc2VydmljZWFjY291bnQuY29tIiwic3ViIjoiZmlyZWJhc2UtYWRtaW5zZGstazVuY3VAbGliZXJ0YXBwLWRldi00ZWM4Zi5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsInVpZCI6ImI0NmY5MDRkLWFjNjgtNDVkMi1hMGM5LTc2MWFmMGZhY2Y5ZCIsImNsYWltcyI6eyJtZXNhcyI6W3sibWVzYUlkIjoiMDAwMDEifSx7Im1lc2FJZCI6IjAwMDAyIn0seyJtZXNhSWQiOiIwMDAwMyJ9LHsibWVzYUlkIjoiMDAwMDQifSx7Im1lc2FJZCI6IjAwMDA1In1dfX0.E75bnM2hIfrleNZUtK-GShln38O1TXgxpb60-LV2mcEsM0pxMk9p7xNWZdeblyQ12opL6zAlU9dCouWhGPVS0gej9JfliPrUeJ9LlcRJyvCkUnxL8OyHtFmFLSGzej0WdD-YLWWv2MuSYNA9XARFZgmI3r7eKTfwRWAewZPZCRRxAI0kzI4uF24pa3XIK6p5hDjBnJG00QAu2L_DtVTiVL1eUsg31jQAYOl4SLcIzV1kxJy5HWHDebUVwn-jlKiMTS1pNV6zO4A2Id_1nP28xARu8V2xxwBbA6UouYkB_6uWiVHQwtmQ3WNcyIhmDKBf_L4aqJWBziTIiUCeIQFAYw',
+        id: '0323-12343-1234-001asdasd23A',
+        status: 'registered',
+        email: 'leilagonzalez@gmail.com',
+        phone: '1203120312',
+    },
+    {
+        fullName: 'Laura Rodriguez',
+        magicLink:
+            'https://app.dev.libertapp.net/?authToken=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2lkZW50aXR5dG9vbGtpdC5nb29nbGVhcGlzLmNvbS9nb29nbGUuaWRlbnRpdHkuaWRlbnRpdHl0b29sa2l0LnYxLklkZW50aXR5VG9vbGtpdCIsImlhdCI6MTcwMDExMDgzOSwiZXhwIjoxNzAwMTE0NDM5LCJpc3MiOiJmaXJlYmFzZS1hZG1pbnNkay1rNW5jdUBsaWJlcnRhcHAtZGV2LTRlYzhmLmlhbS5nc2VydmljZWFjY291bnQuY29tIiwic3ViIjoiZmlyZWJhc2UtYWRtaW5zZGstazVuY3VAbGliZXJ0YXBwLWRldi00ZWM4Zi5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsInVpZCI6ImI0NmY5MDRkLWFjNjgtNDVkMi1hMGM5LTc2MWFmMGZhY2Y5ZCIsImNsYWltcyI6eyJtZXNhcyI6W3sibWVzYUlkIjoiMDAwMDEifSx7Im1lc2FJZCI6IjAwMDAyIn0seyJtZXNhSWQiOiIwMDAwMyJ9LHsibWVzYUlkIjoiMDAwMDQifSx7Im1lc2FJZCI6IjAwMDA1In1dfX0.E75bnM2hIfrleNZUtK-GShln38O1TXgxpb60-LV2mcEsM0pxMk9p7xNWZdeblyQ12opL6zAlU9dCouWhGPVS0gej9JfliPrUeJ9LlcRJyvCkUnxL8OyHtFmFLSGzej0WdD-YLWWv2MuSYNA9XARFZgmI3r7eKTfwRWAewZPZCRRxAI0kzI4uF24pa3XIK6p5hDjBnJG00QAu2L_DtVTiVL1eUsg31jQAYOl4SLcIzV1kxJy5HWHDebUVwn-jlKiMTS1pNV6zO4A2Id_1nP28xARu8V2xxwBbA6UouYkB_6uWiVHQwtmQ3WNcyIhmDKBf_L4aqJWBziTIiUCeIQFAYw',
+        id: '0323-12343-1234-0qweqwe0123C',
+        status: 'pending',
+        email: 'laverodriguez@gmail.com',
+        phone: '1203120312',
+    },
+    {
+        fullName: 'Ignacio Vazquez',
+        magicLink:
+            'https://app.dev.libertapp.net/?authToken=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2lkZW50aXR5dG9vbGtpdC5nb29nbGVhcGlzLmNvbS9nb29nbGUuaWRlbnRpdHkuaWRlbnRpdHl0b29sa2l0LnYxLklkZW50aXR5VG9vbGtpdCIsImlhdCI6MTcwMDExMDgzOSwiZXhwIjoxNzAwMTE0NDM5LCJpc3MiOiJmaXJlYmFzZS1hZG1pbnNkay1rNW5jdUBsaWJlcnRhcHAtZGV2LTRlYzhmLmlhbS5nc2VydmljZWFjY291bnQuY29tIiwic3ViIjoiZmlyZWJhc2UtYWRtaW5zZGstazVuY3VAbGliZXJ0YXBwLWRldi00ZWM4Zi5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsInVpZCI6ImI0NmY5MDRkLWFjNjgtNDVkMi1hMGM5LTc2MWFmMGZhY2Y5ZCIsImNsYWltcyI6eyJtZXNhcyI6W3sibWVzYUlkIjoiMDAwMDEifSx7Im1lc2FJZCI6IjAwMDAyIn0seyJtZXNhSWQiOiIwMDAwMyJ9LHsibWVzYUlkIjoiMDAwMDQifSx7Im1lc2FJZCI6IjAwMDA1In1dfX0.E75bnM2hIfrleNZUtK-GShln38O1TXgxpb60-LV2mcEsM0pxMk9p7xNWZdeblyQ12opL6zAlU9dCouWhGPVS0gej9JfliPrUeJ9LlcRJyvCkUnxL8OyHtFmFLSGzej0WdD-YLWWv2MuSYNA9XARFZgmI3r7eKTfwRWAewZPZCRRxAI0kzI4uF24pa3XIK6p5hDjBnJG00QAu2L_DtVTiVL1eUsg31jQAYOl4SLcIzV1kxJy5HWHDebUVwn-jlKiMTS1pNV6zO4A2Id_1nP28xARu8V2xxwBbA6UouYkB_6uWiVHQwtmQ3WNcyIhmDKBf_L4aqJWBziTIiUCeIQFAYw',
+        id: '0323-12343-1234-00asdasdaszxc123B',
+        status: 'registered',
+        email: 'ignasave39@gmail.com',
+        phone: '1203120312',
+    },
+    {
+        fullName: 'Leila Gonzalez',
+        magicLink:
+            'https://app.dev.libertapp.net/?authToken=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2lkZW50aXR5dG9vbGtpdC5nb29nbGVhcGlzLmNvbS9nb29nbGUuaWRlbnRpdHkuaWRlbnRpdHl0b29sa2l0LnYxLklkZW50aXR5VG9vbGtpdCIsImlhdCI6MTcwMDExMDgzOSwiZXhwIjoxNzAwMTE0NDM5LCJpc3MiOiJmaXJlYmFzZS1hZG1pbnNkay1rNW5jdUBsaWJlcnRhcHAtZGV2LTRlYzhmLmlhbS5nc2VydmljZWFjY291bnQuY29tIiwic3ViIjoiZmlyZWJhc2UtYWRtaW5zZGstazVuY3VAbGliZXJ0YXBwLWRldi00ZWM4Zi5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsInVpZCI6ImI0NmY5MDRkLWFjNjgtNDVkMi1hMGM5LTc2MWFmMGZhY2Y5ZCIsImNsYWltcyI6eyJtZXNhcyI6W3sibWVzYUlkIjoiMDAwMDEifSx7Im1lc2FJZCI6IjAwMDAyIn0seyJtZXNhSWQiOiIwMDAwMyJ9LHsibWVzYUlkIjoiMDAwMDQifSx7Im1lc2FJZCI6IjAwMDA1In1dfX0.E75bnM2hIfrleNZUtK-GShln38O1TXgxpb60-LV2mcEsM0pxMk9p7xNWZdeblyQ12opL6zAlU9dCouWhGPVS0gej9JfliPrUeJ9LlcRJyvCkUnxL8OyHtFmFLSGzej0WdD-YLWWv2MuSYNA9XARFZgmI3r7eKTfwRWAewZPZCRRxAI0kzI4uF24pa3XIK6p5hDjBnJG00QAu2L_DtVTiVL1eUsg31jQAYOl4SLcIzV1kxJy5HWHDebUVwn-jlKiMTS1pNV6zO4A2Id_1nP28xARu8V2xxwBbA6UouYkB_6uWiVHQwtmQ3WNcyIhmDKBf_L4aqJWBziTIiUCeIQFAYw',
+        id: '0323-12343-1234-0xvadfqw0123A',
+        status: 'registered',
+        email: 'leilagonzalez@gmail.com',
+        phone: '1203120312',
+    },
+    {
+        fullName: 'Laura Rodriguez',
+        magicLink:
+            'https://app.dev.libertapp.net/?authToken=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2lkZW50aXR5dG9vbGtpdC5nb29nbGVhcGlzLmNvbS9nb29nbGUuaWRlbnRpdHkuaWRlbnRpdHl0b29sa2l0LnYxLklkZW50aXR5VG9vbGtpdCIsImlhdCI6MTcwMDExMDgzOSwiZXhwIjoxNzAwMTE0NDM5LCJpc3MiOiJmaXJlYmFzZS1hZG1pbnNkay1rNW5jdUBsaWJlcnRhcHAtZGV2LTRlYzhmLmlhbS5nc2VydmljZWFjY291bnQuY29tIiwic3ViIjoiZmlyZWJhc2UtYWRtaW5zZGstazVuY3VAbGliZXJ0YXBwLWRldi00ZWM4Zi5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsInVpZCI6ImI0NmY5MDRkLWFjNjgtNDVkMi1hMGM5LTc2MWFmMGZhY2Y5ZCIsImNsYWltcyI6eyJtZXNhcyI6W3sibWVzYUlkIjoiMDAwMDEifSx7Im1lc2FJZCI6IjAwMDAyIn0seyJtZXNhSWQiOiIwMDAwMyJ9LHsibWVzYUlkIjoiMDAwMDQifSx7Im1lc2FJZCI6IjAwMDA1In1dfX0.E75bnM2hIfrleNZUtK-GShln38O1TXgxpb60-LV2mcEsM0pxMk9p7xNWZdeblyQ12opL6zAlU9dCouWhGPVS0gej9JfliPrUeJ9LlcRJyvCkUnxL8OyHtFmFLSGzej0WdD-YLWWv2MuSYNA9XARFZgmI3r7eKTfwRWAewZPZCRRxAI0kzI4uF24pa3XIK6p5hDjBnJG00QAu2L_DtVTiVL1eUsg31jQAYOl4SLcIzV1kxJy5HWHDebUVwn-jlKiMTS1pNV6zO4A2Id_1nP28xARu8V2xxwBbA6UouYkB_6uWiVHQwtmQ3WNcyIhmDKBf_L4aqJWBziTIiUCeIQFAYw',
+        id: '0323-12343-1234-001rtqweqw23C',
+        status: 'pending',
+        email: 'laverodriguez@gmail.com',
+        phone: '1203120312',
+    },
+];
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,8 +1,7 @@
 import {
-    Accordion,
-    AccordionItem,
     Button,
     Chip,
+    Pagination,
     Snippet,
     Table,
     TableBody,
@@ -15,9 +14,11 @@ import {
 } from '@nextui-org/react';
 import { Link } from 'react-router-dom';
 import DeleteFiscalModal from './deleteFiscalModal';
-import { useCallback, useState } from 'react';
+import { Key, useCallback, useMemo, useState } from 'react';
 import EditIcon from '../../assets/EditIcon';
 import DeleteIcon from '../../assets/DeleteIcon';
+import { dummyFiscales } from './data';
+import { Fiscal } from '../../entities/Fiscal';
 
 const columns = [
     { name: 'Nombre Completo', uid: 'fullName' },
@@ -25,45 +26,6 @@ const columns = [
     { name: 'Telefono', uid: 'phone' },
     { name: 'Estado', uid: 'status' },
     { name: 'Acciones', uid: 'actions' },
-];
-
-interface Fiscal {
-    fullName: string;
-    magicLink: string;
-    id: string;
-    status: string;
-    email: string;
-    phone: string;
-}
-
-const dummyFiscales = [
-    {
-        fullName: 'Ignacio Vazquez',
-        magicLink:
-            'https://app.dev.libertapp.net/?authToken=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2lkZW50aXR5dG9vbGtpdC5nb29nbGVhcGlzLmNvbS9nb29nbGUuaWRlbnRpdHkuaWRlbnRpdHl0b29sa2l0LnYxLklkZW50aXR5VG9vbGtpdCIsImlhdCI6MTcwMDExMDgzOSwiZXhwIjoxNzAwMTE0NDM5LCJpc3MiOiJmaXJlYmFzZS1hZG1pbnNkay1rNW5jdUBsaWJlcnRhcHAtZGV2LTRlYzhmLmlhbS5nc2VydmljZWFjY291bnQuY29tIiwic3ViIjoiZmlyZWJhc2UtYWRtaW5zZGstazVuY3VAbGliZXJ0YXBwLWRldi00ZWM4Zi5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsInVpZCI6ImI0NmY5MDRkLWFjNjgtNDVkMi1hMGM5LTc2MWFmMGZhY2Y5ZCIsImNsYWltcyI6eyJtZXNhcyI6W3sibWVzYUlkIjoiMDAwMDEifSx7Im1lc2FJZCI6IjAwMDAyIn0seyJtZXNhSWQiOiIwMDAwMyJ9LHsibWVzYUlkIjoiMDAwMDQifSx7Im1lc2FJZCI6IjAwMDA1In1dfX0.E75bnM2hIfrleNZUtK-GShln38O1TXgxpb60-LV2mcEsM0pxMk9p7xNWZdeblyQ12opL6zAlU9dCouWhGPVS0gej9JfliPrUeJ9LlcRJyvCkUnxL8OyHtFmFLSGzej0WdD-YLWWv2MuSYNA9XARFZgmI3r7eKTfwRWAewZPZCRRxAI0kzI4uF24pa3XIK6p5hDjBnJG00QAu2L_DtVTiVL1eUsg31jQAYOl4SLcIzV1kxJy5HWHDebUVwn-jlKiMTS1pNV6zO4A2Id_1nP28xARu8V2xxwBbA6UouYkB_6uWiVHQwtmQ3WNcyIhmDKBf_L4aqJWBziTIiUCeIQFAYw',
-        id: '0323-12343-1234-00123B',
-        status: 'registered',
-        email: 'ignasave39@gmail.com',
-        phone: '1203120312',
-    },
-    {
-        fullName: 'Leila Gonzalez',
-        magicLink:
-            'https://app.dev.libertapp.net/?authToken=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2lkZW50aXR5dG9vbGtpdC5nb29nbGVhcGlzLmNvbS9nb29nbGUuaWRlbnRpdHkuaWRlbnRpdHl0b29sa2l0LnYxLklkZW50aXR5VG9vbGtpdCIsImlhdCI6MTcwMDExMDgzOSwiZXhwIjoxNzAwMTE0NDM5LCJpc3MiOiJmaXJlYmFzZS1hZG1pbnNkay1rNW5jdUBsaWJlcnRhcHAtZGV2LTRlYzhmLmlhbS5nc2VydmljZWFjY291bnQuY29tIiwic3ViIjoiZmlyZWJhc2UtYWRtaW5zZGstazVuY3VAbGliZXJ0YXBwLWRldi00ZWM4Zi5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsInVpZCI6ImI0NmY5MDRkLWFjNjgtNDVkMi1hMGM5LTc2MWFmMGZhY2Y5ZCIsImNsYWltcyI6eyJtZXNhcyI6W3sibWVzYUlkIjoiMDAwMDEifSx7Im1lc2FJZCI6IjAwMDAyIn0seyJtZXNhSWQiOiIwMDAwMyJ9LHsibWVzYUlkIjoiMDAwMDQifSx7Im1lc2FJZCI6IjAwMDA1In1dfX0.E75bnM2hIfrleNZUtK-GShln38O1TXgxpb60-LV2mcEsM0pxMk9p7xNWZdeblyQ12opL6zAlU9dCouWhGPVS0gej9JfliPrUeJ9LlcRJyvCkUnxL8OyHtFmFLSGzej0WdD-YLWWv2MuSYNA9XARFZgmI3r7eKTfwRWAewZPZCRRxAI0kzI4uF24pa3XIK6p5hDjBnJG00QAu2L_DtVTiVL1eUsg31jQAYOl4SLcIzV1kxJy5HWHDebUVwn-jlKiMTS1pNV6zO4A2Id_1nP28xARu8V2xxwBbA6UouYkB_6uWiVHQwtmQ3WNcyIhmDKBf_L4aqJWBziTIiUCeIQFAYw',
-        id: '0323-12343-1234-00123A',
-        status: 'registered',
-        email: 'leilagonzalez@gmail.com',
-        phone: '1203120312',
-    },
-    {
-        fullName: 'Laura Rodriguez',
-        magicLink:
-            'https://app.dev.libertapp.net/?authToken=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2lkZW50aXR5dG9vbGtpdC5nb29nbGVhcGlzLmNvbS9nb29nbGUuaWRlbnRpdHkuaWRlbnRpdHl0b29sa2l0LnYxLklkZW50aXR5VG9vbGtpdCIsImlhdCI6MTcwMDExMDgzOSwiZXhwIjoxNzAwMTE0NDM5LCJpc3MiOiJmaXJlYmFzZS1hZG1pbnNkay1rNW5jdUBsaWJlcnRhcHAtZGV2LTRlYzhmLmlhbS5nc2VydmljZWFjY291bnQuY29tIiwic3ViIjoiZmlyZWJhc2UtYWRtaW5zZGstazVuY3VAbGliZXJ0YXBwLWRldi00ZWM4Zi5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsInVpZCI6ImI0NmY5MDRkLWFjNjgtNDVkMi1hMGM5LTc2MWFmMGZhY2Y5ZCIsImNsYWltcyI6eyJtZXNhcyI6W3sibWVzYUlkIjoiMDAwMDEifSx7Im1lc2FJZCI6IjAwMDAyIn0seyJtZXNhSWQiOiIwMDAwMyJ9LHsibWVzYUlkIjoiMDAwMDQifSx7Im1lc2FJZCI6IjAwMDA1In1dfX0.E75bnM2hIfrleNZUtK-GShln38O1TXgxpb60-LV2mcEsM0pxMk9p7xNWZdeblyQ12opL6zAlU9dCouWhGPVS0gej9JfliPrUeJ9LlcRJyvCkUnxL8OyHtFmFLSGzej0WdD-YLWWv2MuSYNA9XARFZgmI3r7eKTfwRWAewZPZCRRxAI0kzI4uF24pa3XIK6p5hDjBnJG00QAu2L_DtVTiVL1eUsg31jQAYOl4SLcIzV1kxJy5HWHDebUVwn-jlKiMTS1pNV6zO4A2Id_1nP28xARu8V2xxwBbA6UouYkB_6uWiVHQwtmQ3WNcyIhmDKBf_L4aqJWBziTIiUCeIQFAYw',
-        id: '0323-12343-1234-00123C',
-        status: 'pending',
-        email: 'laverodriguez@gmail.com',
-        phone: '1203120312',
-    },
 ];
 
 const statusColorMap = {
@@ -79,16 +41,20 @@ const Dashboard = () => {
         setDeleteFiscalID(fiscalID);
         onOpen();
     };
-    //TODO: inferir el tipo de key de fiscal
-    const renderCell = useCallback((fiscal: Fiscal, columnKey: string) => {
-        const cellValue = fiscal[columnKey];
+
+    const renderCell = useCallback((fiscal: Fiscal, columnKey: Key) => {
+        const cellValue = fiscal[columnKey as keyof typeof fiscal];
 
         switch (columnKey) {
             case 'name':
                 return <span>{fiscal.fullName}</span>;
+            case 'email':
+                return <span>{fiscal.email}</span>;
+            case 'phone':
+                return <span>{fiscal.phone}</span>;
             case 'status':
                 return (
-                    <Chip className="capitalize" color={statusColorMap[fiscal.status]} variant="flat">
+                    <Chip className="capitalize" color={statusColorMap[fiscal.status] as 'success' | 'danger'} variant="flat">
                         {cellValue}
                     </Chip>
                 );
@@ -102,7 +68,7 @@ const Dashboard = () => {
                             <Link to={'/edit-fiscal'}>
                                 <span className="text-xl text-default-400 cursor-pointer active:opacity-50">
                                     <EditIcon />
-                                </span>{' '}
+                                </span>
                             </Link>
                         </Tooltip>
                         <Tooltip color="danger" content="Eliminar Fiscal">
@@ -117,6 +83,18 @@ const Dashboard = () => {
         }
     }, []);
 
+    const [page, setPage] = useState(1);
+    const rowsPerPage = 4;
+
+    const pages = Math.ceil(dummyFiscales.length / rowsPerPage);
+
+    const items = useMemo(() => {
+        const start = (page - 1) * rowsPerPage;
+        const end = start + rowsPerPage;
+
+        return dummyFiscales.slice(start, end);
+    }, [page]);
+
     return (
         <div className="flex items-center justify-center flex-col">
             <span>Dashboard</span>
@@ -125,7 +103,22 @@ const Dashboard = () => {
             </Link>
             <div>
                 Fiscales
-                <Table aria-label="Example table with custom cells">
+                <Table
+                    aria-label="Example table with custom cells"
+                    bottomContent={
+                        <div className="flex w-full justify-center">
+                            <Pagination
+                                isCompact
+                                showControls
+                                showShadow
+                                color="secondary"
+                                page={page}
+                                total={pages}
+                                onChange={(page) => setPage(page)}
+                            />
+                        </div>
+                    }
+                >
                     <TableHeader columns={columns}>
                         {(column) => (
                             <TableColumn key={column.uid} align={column.uid === 'actions' ? 'center' : 'start'}>
@@ -133,7 +126,7 @@ const Dashboard = () => {
                             </TableColumn>
                         )}
                     </TableHeader>
-                    <TableBody items={dummyFiscales}>
+                    <TableBody items={items}>
                         {(item) => <TableRow key={item.id}>{(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}</TableRow>}
                     </TableBody>
                 </Table>
